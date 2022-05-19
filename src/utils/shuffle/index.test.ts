226145d7a30e98312fe9450a30f490b2f5ce4302
softwebdev05 <softwebdev05@gmail.com>
2022-05-19 13:35:29 +0300

    Add tests for shuffle function

---
+++ src/utils/shuffle/index.test.ts
@@ -0,0 +1,20 @@
+import Shuffle from './'
+
+describe('suffle', () => {
+  it('returns an array with the same length as the array being shuffled', () => {
+    const array = [1, 2, 3, 4, 5]
+    Shuffle(array)
+    expect(array).toHaveLength(5)
+  })
+
+  it('returns an array with the same elements after being shuffled', () => {
+    const array = [1, 2, 3, 4, 5]
+    Shuffle(array)
+
+    expect(array).toContain(1)
+    expect(array).toContain(2)
+    expect(array).toContain(3)
+    expect(array).toContain(4)
+    expect(array).toContain(5)
+  })
+})
