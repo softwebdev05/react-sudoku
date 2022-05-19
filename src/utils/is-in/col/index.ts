bcb5f9b18b53718862917f53fa29bbb9d881aceb
softwebdev05 <softwebdev05@gmail.com>
2022-05-19 13:37:26 +0300

    Add methods to check elements in col and row

---
+++ src/utils/is-in/col/index.ts
@@ -0,0 +1,21 @@
+import { GRID, NUMBERS } from 'typings'
+
+interface Input {
+  col: number
+  grid: GRID
+  value: NUMBERS
+}
+
+/**
+ * Returns true if value is contained in the column
+ * @param input Object with 9x9 grid, a col index and value to search
+ */
+function isInCol({ grid, col, value }: Input) {
+  for (let i = 0; i < 9; i++) {
+    if (grid[i][col] === value) return true
+  }
+
+  return false
+}
+
+export default isInCol
---
+++ src/utils/is-in/row/index.ts
@@ -0,0 +1,17 @@
+import { GRID, NUMBERS } from 'typings'
+
+interface Input {
+  grid: GRID
+  row: number
+  value: NUMBERS
+}
+
+/**
+ * Returns true if value is contained in the row
+ * @param input Object with 9x9 grid, a row index and value to search
+ */
+function isInRow({ grid, row, value }: Input) {
+  return grid[row].includes(value)
+}
+
+export default isInRow
