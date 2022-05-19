95fee37b4711ad85cf4cd54edf140a779302b682
softwebdev05 <softwebdev05@gmail.com>
2022-05-19 13:31:05 +0300

    Set base URL for TS modules

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { ThemeProvider } from 'styled-components'
-import { Card, Content, Grid, Title } from './components'
-import { GlobalStyles, theme } from '../styles'
+import { Card, Content, Grid, Title } from 'components'
+import { GlobalStyles, theme } from 'styles'
+
+
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
