3a4c1d7ed6a50518b480524230d284a6e9636e11
softwebdev05 <softwebdev05@gmail.com>
2022-05-19 14:11:57 +0300

    Add default reducer

---
+++ src/reducers/reducer.ts
@@ -0,0 +1,21 @@
+import { AnyAction } from 'redux'
+import { createFullGrid } from 'utils'
+
+import { Reducer } from './interfaces'
+import * as types from './types'
+
+const initialState = {}
+
+function reducer(state = initialState, action: AnyAction): Reducer {
+  switch (action.type) {
+    case types.CREATE_GRID:
+      return {
+        ...state,
+        grid: createFullGrid(),
+      }
+    default:
+      return state
+  }
+}
+
+export default reducer
