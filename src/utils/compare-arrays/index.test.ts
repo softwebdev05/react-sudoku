fb1f5b09e35c1f46533243cff6fba06aa4bc0306
softwebdev05 <softwebdev05@gmail.com>
2022-05-20 12:26:22 +0300

    Add compare arrays function

---
+++ src/utils/compare-arrays/index.test.ts
@@ -0,0 +1,32 @@
+import compareArrays from './'
+
+describe('compareArrays', () => {
+  it('returns true if arrays are identical', () => {
+    expect(compareArrays([1, 2, 3, 4], [1, 2, 3, 4])).toBeTruthy()
+
+    expect(compareArrays([1, '2', 3, '4'], [1, '2', 3, '4'])).toBeTruthy()
+
+    expect(compareArrays([[1, '2', 3, '4']], [[1, '2', 3, '4']])).toBeTruthy()
+
+    expect(
+      compareArrays(
+        [
+          [1, '2', 3, '4'],
+          [5, 6, null, undefined],
+        ],
+        [
+          [1, '2', 3, '4'],
+          [5, 6, null, undefined],
+        ]
+      )
+    ).toBeTruthy()
+  })
+
+  it('returns false if arrays are identical not', () => {
+    expect(compareArrays([1, 2, 3, 4], [1, 2, 3])).toBeFalsy()
+
+    expect(compareArrays([1, '2', 3, '4', '5'], [1, '2', 3, '4', 5])).toBeFalsy()
+
+    expect(compareArrays([[1, '2', 3, '4'], [5]], [[1, '2', 3, '4']])).toBeFalsy()
+  })
+})
---
+++ src/utils/compare-arrays/index.ts
@@ -0,0 +1,32 @@
+/**
+ * Returns true if both values are arrays and they contain the same elements.
+ *
+ * This function compares arrays with any number of dimensions.
+ *
+ * Elements are compared for strict equality.
+ *
+ * @param arr1 An array to compare to
+ * @param arr2 An array to compare with
+ */
+ function compareArrays(arr1: any[], arr2: any[]): boolean {
+  if (arr1.length !== arr2.length) return false
+
+  for (let i = 0, len = arr1.length; i < len; i++) {
+    const element1 = arr1[i]
+    const element2 = arr2[i]
+    const element1IsArray = Array.isArray(element1)
+    const element2IsArray = Array.isArray(element2)
+
+    if (!element1IsArray || !element2IsArray) {
+      if (element2IsArray || element2IsArray) return false
+
+      if (element1 === element2) continue
+    }
+
+    if (!compareArrays(element1, element2)) return false
+  }
+
+  return true
+}
+
+export default compareArrays
