2bc4efb0cc15753b8fd126bbd31ab703e5db2520
softwebdev05 <softwebdev05@gmail.com>
2022-05-20 12:52:07 +0300

    Enable fill numbers with buttons to support mobile

--- src/components/numbers/button/index.tsx
+++ src/components/numbers/button/index.tsx
@@ -1,16 +1,34 @@
-import React, { FC } from 'react'
+import React, { Dispatch, FC, useCallback } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
 import { Button } from 'components'
-import { NUMBERS } from 'typings'
+import { BLOCK_COORDS, N, NUMBERS } from 'typings'
+import { fillBlock, Reducer } from 'reducers'
+import { AnyAction } from 'redux'
 
 interface Props {
   value: NUMBERS
 }
 
+interface State {
+  selectedBlock?: BLOCK_COORDS
+  selectedValue: N
+}
+
 const NumberButton: FC<Props> = ({ value }) => {
-  const fill = () => {
-    console.log('fill', value)
-  }
+  const state = useSelector<Reducer, State>(({ selectedBlock, workingGrid }) => ({
+    selectedBlock,
+    selectedValue:
+      workingGrid && selectedBlock ? workingGrid[selectedBlock[0]][selectedBlock[1]] : 0,
+  }))
+
+  const dispatch = useDispatch<Dispatch<AnyAction>>()
+
+  const fill = useCallback(() => {
+    if (state.selectedBlock && state.selectedValue === 0) {
+      dispatch(fillBlock(state.selectedBlock, value))
+    }
+  }, [dispatch, state.selectedBlock, state.selectedValue, value])
 
   return <Button onClick={fill}>{value}</Button>
 }
